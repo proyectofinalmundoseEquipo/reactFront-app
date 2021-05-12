31e28867a50caa948ce8cd53745ce6f300595f69
proyectofinalmundoseEquipo <83417744+proyectofinalmundoseEquipo@users.noreply.github.com>
2021-05-12 19:05:24 -0300

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: 'http://trabajo-final-laravel-back.herokuapp.com/api/'
+  baseURL: 'https://trabajo-final-laravel-back.herokuapp.com/api/'
 })
 
 
